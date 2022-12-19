e8ae8bbacb7ef6238b1ca5b834a9bd482f4120ce
atostenson13 <115951011+atostenson13@users.noreply.github.com>
2022-12-18 21:26:50 -0800

    Added href to github code

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+import Search from "./Search";
+
+export default function App() {
+  return (
+    <div className="App">
+      <a href="https://www.npmjs.com/package/react-animated-weather"></a>
+      <h1>Weather Search Engine</h1>
+      <br />
+      <Search />
+      <a href="https://github.com/atostenson13/weather-app-react-1.0">Github Codea</a>
+    </div>
+  );
+}
